e288928c1a0e14e328ccbc8c9d52cf64b56b4936
kvellur <karthik_vellur@intuit.com>
2020-03-23 14:32:34 +0530

    feat: added initial navigation

--- App.js
+++ App.js
@@ -1,22 +1,61 @@
-import React, { Component } from 'react'
-import { createStore  } from 'redux'
+
+import React from 'react'
+import { View, Platform } from 'react-native'
+import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import { 
-  View,
-  StyleSheet
-} from 'react-native'
-import AddEntry from './components/AddEntry'
 import History from './components/History'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { purple, white } from './utils/colors'
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
 
+const TabNavigator = createMaterialTopTabNavigator(
+  {
+    History: History,
+    AddEntry: AddEntry,
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return routeName === 'History' ? (
+          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+        ) : (
+          <FontAwesome name="plus-square" size={30} color={tintColor} />
+        );
+      },
+    }),
+    tabBarOptions: {
+      showIcon: true,
+      activeTintColor: Platform.OS === 'ios' ? purple : white,
+      style: {
+        height: 66,
+        backgroundColor: Platform.OS === 'ios' ? white : purple,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+      },
+    },
+  }
+);
 
-export default class App extends Component {
+const Tabs = createAppContainer(TabNavigator)
 
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View style={{flex:1}}>
-            <History/>
+        <View style={{ flex: 1 }} >
+          <View style={{ height: 20 }} />
+          <Tabs />
         </View>
       </Provider>
     )
